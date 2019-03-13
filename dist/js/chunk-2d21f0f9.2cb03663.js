7d6edd1f860795bf56b0a7dccee517edfad4468c
sy2729 <sy2729@tc.columbia.edu>
2019-03-13 19:15:26 -0400

    unignore the dist folder

---
+++ dist/js/chunk-2d21f0f9.2cb03663.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21f0f9"],{d7e5:function(e,o,n){"use strict";n.r(o);var a={"2019-03-13":{content:[{name:"227市场",detail:"定价依据和客户判定",role:{name:"经办人",code:0},time:"16:24",more:"lorem"},{name:"新软件",detail:"成立体验小组，测试软件",role:{name:"交办人",code:1},time:"13:34",more:"lorem"}]}};o["default"]=a}}]);
+//# sourceMappingURL=chunk-2d21f0f9.2cb03663.js.map
